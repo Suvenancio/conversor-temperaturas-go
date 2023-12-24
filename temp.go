3127d65510bfc09917f91f5816138ba5e15b5891
Suvenancio <venanciosuh@gmail.com>
2023-12-23 23:11:34 -0300

    feat: cria program para converter temperatura kelvin em celsius

---
+++ temp.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+ 
+func main () {
+		ebulicaoK := 373
+		ebulicaoC := ebulicaoK - 273
+
+	fmt.Printf("A temperatura de ebulição da água em graus kelvin é igual a %v e em em graus celsius é igual a %v", ebulicaoK, ebulicaoC)
+}
